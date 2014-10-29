09394735d9efc3c0d21fccdf911f7110255e0c66
brousalis <brousapg@gmail.com>
2014-10-29 10:23:29 -0500

    finally starting on ui

--- Gemfile
+++ Gemfile
@@ -1,11 +1,5 @@
 source 'https://rubygems.org'
 
-gem 'coffee-script'
-gem 'compass'
 gem 'jekyll'
-gem 'jekyll-assets'
-gem 'rake'
-gem 'sass'
-gem 'uglifier'
 gem 'github-pages'
 gem 'bootstrap-sass'
--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,8 +1,3 @@
-require "compass"
-require "sass"
-require "uglifier"
-require "coffee-script"
-
 ::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
 
 require "bootstrap-sass"
