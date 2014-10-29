926786067fea78327842b44dbce11f406256dcc3
Pete Brousalis <pbrousalis@enova.com>
2014-10-29 09:34:45 -0500

    lol, removed asset piepline. changed my mind

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,10 +1,9 @@
-require 'compass'
-require 'sass'
-require 'uglifier'
-require 'coffee-script'
+require "compass"
+require "sass"
+require "uglifier"
+require "coffee-script"
 
 ::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
 
-require 'jekyll-assets'
-require 'jekyll-assets/compass'
-require 'jekyll-assets/bootstrap'
+require "bootstrap-sass"
+
