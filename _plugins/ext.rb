163789d0a58db8af940363a81f8aed8351e31a5a
Pete Brousalis <pbrousalis@enova.com>
2014-10-29 01:02:14 -0500

    asset pipeline for jekyll, hmmmmme for jekyll, not sure about it

---
+++ _plugins/ext.rb
@@ -0,0 +1,10 @@
+require 'compass'
+require 'sass'
+require 'uglifier'
+require 'coffee-script'
+
+::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
+
+require 'jekyll-assets'
+require 'jekyll-assets/compass'
+require 'jekyll-assets/bootstrap'
