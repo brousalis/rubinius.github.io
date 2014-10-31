1e08d58a092d27eefe2d35b8585874971752e37d
brousalis <brousapg@gmail.com>
2014-10-30 22:26:25 -0500

    well thats fancier

--- javascripts/application.js
+++ javascripts/application.js
@@ -9,3 +9,30 @@ $(document).ready(function() {
     showProfileLink: true
   });
 });
+
+! function(a) {
+  "use strict";
+  a(function() {
+    var b = a(window),
+        c = a(document.body);
+
+    c.scrollspy({
+      target: ".affix"
+    }), 
+    b.on("load", function() {
+      c.scrollspy("refresh")
+    })
+
+    var b = a(".affix");
+        b.affix({
+          offset: {
+            top: function() {
+              var c = b.offset().top,
+                d = parseInt(b.children(0).css("margin-top"), 10),
+                e = a("nav").height();
+              return this.top = c - e - d
+            },
+          }
+        })
+      })
+}(jQuery);
