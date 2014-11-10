d7f9b702c579ee9a0404eb520849ac6911d53d82
brousalis <brousapg@gmail.com>
2014-11-09 20:58:14 -0600

    more progress on docs

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,38 +1,17 @@
-$(document).ready(function() {
-  $("#twitter").getTwitter({
-    userName: "rubinius",
-    numTweets: 6,
-    loaderText: "Loading @Rubinius Tweets&hellip;",
-    slideIn: false,
-    showHeading: false,
-    headingText: "Latest Tweets",
-    showProfileLink: true
-  });
+$(function() {
+  if($('body').find('.doc-wrapper')) {
+    $('#search-query').lunrSearch({
+      indexUrl: '/search.json',
+      results:  '#search-results',
+      entries:  '.entries',
+      template: '#search-results-template'
+    });
+
+    var path = window.location.pathname;
+    $('.sidebar-links a[href="'+path+'"]').addClass('active');
+
+    $('.languages').click(function() { 
+      $(this).toggleClass('open');
+    });
+  }
 });
-
-! function(a) {
-  "use strict";
-  a(function() {
-    var b = a(window),
-        c = a(document.body);
-
-    c.scrollspy({
-      target: ".affix"
-    }), 
-    b.on("load", function() {
-      c.scrollspy("refresh")
-    })
-
-    var b = a(".affix");
-        b.affix({
-          offset: {
-            top: function() {
-              var c = b.offset().top,
-                d = parseInt(b.children(0).css("margin-top"), 10),
-                e = a("nav").height();
-              return this.top = c - e - d
-            },
-          }
-        })
-      })
-}(jQuery);
